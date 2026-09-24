79501a50b601ec2a24498756ece02bceefdaa331
Rafael <rafaelalmeidanh@gmail.com>
2026-09-24 19:22:37 -0300

    Projeto tocador de musica

---
+++ src/main/java/com/mycompany/projetotocadordemusica/ProjetoTocadorDeMusica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetotocadordemusica;
+
+/**
+ *
+ * @author rafae_x4pciw0
+ */
+public class ProjetoTocadorDeMusica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
